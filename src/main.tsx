0c0365389cb3e65b18431d5b1ca3e54475ab666b
Cory House <housecor@gmail.com>
2022-07-20 18:44:02 -0500

    Use tailwind in entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import DevTools from "./DevTools";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <p>
-      This file merely exists to display the components. This file isn't bundled
-      in the library.
+    <p className="text-3xl">
+      This file merely exists to display the components. It's not bundled in the
+      library.
     </p>
     <DevTools>
       <input type="checkbox" />
