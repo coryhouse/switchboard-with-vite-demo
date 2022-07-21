97bb745c84f1ed43a3233858ef33644ec1e8455f
Cory House <housecor@gmail.com>
2022-07-21 17:47:49 -0500

    Add test

---
+++ src/utils/urlUtils.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+import { getDevToolsSettingsFromUrlQuerystring } from "./urlUtils";
+
+describe("urlUtils", () => {
+  describe("getDevToolsSettingsFromUrlQuerystring", () => {
+    describe("given the URL's querystring contains settings under the 'devtools' key", () => {
+      it("returns the settings as an object", () => {
+        const settings = { showUser: true, eatChili: false };
+        const encodedQuerystring = window.encodeURI(JSON.stringify(settings));
+        const urlQuery = "?settingToIgnore=true&devtools=" + encodedQuerystring;
+        const result = getDevToolsSettingsFromUrlQuerystring(urlQuery);
+        expect(JSON.stringify(result)).toEqual(JSON.stringify(settings));
+      });
+    });
+  });
+});
