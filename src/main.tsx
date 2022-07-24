6df3e9f45b05aaa68f7b818f1a5e845253aeae7e
Cory House <housecor@gmail.com>
2022-07-24 10:38:47 -0500

    Refactor entry

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import AppWithDevTools from "./demo-app/AppWithDevTools";
 import "./index.css";
 
 // This is set in the dev npm script.
-const showDevTools = import.meta.env.REACT_APP_SHOW_DEV_TOOLS === "Y";
+const useDevTools = import.meta.env.USE_DEV_TOOLS === "Y";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  showDevTools ? <AppWithDevTools /> : <App />
+  useDevTools ? <AppWithDevTools /> : <App />
 );
