eb8db525d6e57f337df67a7972f90a50e218485e
Cory House <housecor@gmail.com>
2022-07-24 11:22:41 -0500

    Configure msw

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -1,13 +1,39 @@
 import App from "./App";
 import DevTools from "../DevTools";
+import { useWorker } from "../useWorker";
+import { DevToolsConfig } from "./types";
+
+const devToolsConfig: DevToolsConfig = {
+  apiResponse: {
+    addTodo: "success",
+    getTodos: "many",
+    markTodoCompleted: "success",
+  },
+  httpDelay: 0,
+  user: {
+    id: 1,
+    name: "Cory",
+    todos: [
+      {
+        id: 1,
+        todo: "Eat chili",
+        completed: false,
+      },
+    ],
+  },
+};
 
 export default function AppWithDevTools() {
-  return (
+  const isReady = useWorker(devToolsConfig);
+
+  return isReady ? (
     <>
       <App />
-      <DevTools settings={}>
+      <DevTools>
         <input type="checkbox" />
       </DevTools>
     </>
+  ) : (
+    <p>Initializing mock service worker</p>
   );
 }
