4268c8429b2efdd1d1c2421d09a0cfb558aabbde
Cory House <housecor@gmail.com>
2022-07-25 06:43:07 -0700

    Centralize

--- cypress/e2e/todo-app.cy.ts
+++ cypress/e2e/todo-app.cy.ts
@@ -1,18 +1,29 @@
-function findWithinSectionByHeading() {}
+// Use parent to search within the heading's <section>
+function isInSection(heading: string, text: string) {
+  cy.findByRole("heading", { name: heading })
+    .parent()
+    .within(() => {
+      cy.findByText(text);
+    });
+}
 
 describe("Todo App", () => {
-  it("shows existing todos on initial load, and supports adding a todo", () => {
+  it("shows existing todos on initial load, supports adding a todo, and marking as complete", () => {
     cy.visit("http://127.0.0.1:5173/");
-    cy.findByRole("heading", { name: "To dos" })
-      // Use parent to search within the heading's <section>
-      .parent()
-      .within(() => {
-        cy.findByText("Eat lunch");
-      });
+
+    // First, assure existing todos display
+    isInSection("Stuff to do", "Eat lunch");
+
+    // Now, add a new todo
     cy.findByLabelText("What do you need to do?").type("Write more tests");
     cy.findByRole("button", { name: "Add" }).click();
-    // Should be cleared after submission
+
+    // New todo should display
+    isInSection("Stuff to do", "Write more tests");
+
+    // Input should be cleared after submission
     cy.findByLabelText("What do you need to do?").should("be.empty");
-    cy.findByText("Write more tests");
+
+    //
   });
 });
