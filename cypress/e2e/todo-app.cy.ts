6682867897d4b6d161687b8440b5cd5a3794ea5e
Cory House <housecor@gmail.com>
2022-07-25 06:18:17 -0700

    Add tests

--- cypress/e2e/todo-app.cy.ts
+++ cypress/e2e/todo-app.cy.ts
@@ -1,5 +1,7 @@
+function findWithinSectionByHeading() {}
+
 describe("Todo App", () => {
-  it("shows existing todos on initial load", () => {
+  it("shows existing todos on initial load, and supports adding a todo", () => {
     cy.visit("http://127.0.0.1:5173/");
     cy.findByRole("heading", { name: "To dos" })
       // Use parent to search within the heading's <section>
@@ -7,5 +9,10 @@ describe("Todo App", () => {
       .within(() => {
         cy.findByText("Eat lunch");
       });
+    cy.findByLabelText("What do you need to do?").type("Write more tests");
+    cy.findByRole("button", { name: "Add" }).click();
+    // Should be cleared after submission
+    cy.findByLabelText("What do you need to do?").should("be.empty");
+    cy.findByText("Write more tests");
   });
 });
