f88183e15ec2d3b6a989972ae3d0123a18031a29
Cory House <housecor@gmail.com>
2022-07-25 06:16:13 -0700

    Fix color

--- src/demo-app/Button.tsx
+++ src/demo-app/Button.tsx
@@ -3,7 +3,7 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 export default function Button(props: ButtonProps) {
   return (
     <button
-      className="border border-slate-400 p-1 bg-blue-400 rounded"
+      className="border border-slate-400 p-1 bg-blue-400 rounded text-white"
       {...props}
     />
   );
