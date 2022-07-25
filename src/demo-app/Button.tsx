180a302048553ca9d532d063c41f440a8673b627
Cory House <housecor@gmail.com>
2022-07-25 05:53:04 -0700

    Add style

--- src/demo-app/Button.tsx
+++ src/demo-app/Button.tsx
@@ -2,6 +2,9 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 
 export default function Button(props: ButtonProps) {
   return (
-    <button className="border border-slate-400 p-1 bg-blue-400" {...props} />
+    <button
+      className="border border-slate-400 p-1 bg-blue-400 rounded"
+      {...props}
+    />
   );
 }
