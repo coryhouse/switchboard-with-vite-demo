4eb04d98a40f330ca75239e9565916f1914c0d07
Cory House <housecor@gmail.com>
2022-07-25 06:33:55 -0700

    Fix args

--- src/demo-app/Input.tsx
+++ src/demo-app/Input.tsx
@@ -4,16 +4,19 @@ interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
 }
 
 export default function Input(props: InputProps) {
+  const { id, onChange, value, ...rest } = props;
   return (
     <>
-      <label className="block" htmlFor={props.id}>
+      <label className="block" htmlFor={id}>
         {props.label}
       </label>
       <input
         className="border-slate-400 border-solid border p-1 rounded"
         type="text"
-        value={props.value}
-        onChange={props.onChange}
+        id={id}
+        value={value}
+        onChange={onChange}
+        {...rest}
       />
     </>
   );
