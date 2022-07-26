c306020b7aa59ecda2e94fe499efd539a2bb9614
Cory House <housecor@gmail.com>
2022-07-26 06:18:33 -0700

    Read config via ref

--- src/useWorker.ts
+++ src/useWorker.ts
@@ -1,15 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { setupWorker, rest, SetupWorkerApi } from "msw";
 import { DevToolsConfig, Todo } from "./demo-app/types";
 import { getRandomNumberBelow } from "./utils/numberUtils";
 
 export const useWorker = (config: DevToolsConfig | null) => {
+  const configRef = useRef(config);
   const [isReady, setIsReady] = useState(false);
+
+  // Store the config in a ref so the useEffect below that starts
+  // the worker runs only once, yet reads the latest config values
+  // as they change in the devtools.
+  useEffect(() => {
+    configRef.current = config;
+  }, [config]);
 
   useEffect(() => {
     const worker = setupWorker(
       rest.get("/todos/:userId", (_req, res, ctx) => {
-        return res(ctx.delay(config?.httpDelay), ctx.json(config?.user.todos));
+        return res(
+          ctx.delay(configRef.current?.httpDelay),
+          ctx.json(configRef.current?.user.todos)
+        );
       }),
       rest.post("/todo", async (req, res, ctx) => {
         const { todo } = await req.json();
@@ -19,10 +30,10 @@ export const useWorker = (config: DevToolsConfig | null) => {
           completed: false,
           todo: todo as string,
         };
-        return res(ctx.delay(config?.httpDelay), ctx.json(resp));
+        return res(ctx.delay(configRef.current?.httpDelay), ctx.json(resp));
       }),
       rest.post("/todo/:id", async (req, res, ctx) => {
-        return res(ctx.delay(config?.httpDelay), ctx.status(200));
+        return res(ctx.delay(configRef.current?.httpDelay), ctx.status(200));
       })
     );
 
