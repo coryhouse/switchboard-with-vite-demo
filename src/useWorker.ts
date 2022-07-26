b4b3b5f5d4ad5ae5e88c0c589d62c1f3727c70bb
Cory House <housecor@gmail.com>
2022-07-26 05:30:37 -0700

    remove import

--- src/useWorker.ts
+++ src/useWorker.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { setupWorker, rest, SetupWorkerApi } from "msw";
 import { DevToolsConfig, Todo } from "./demo-app/types";
 import { getRandomNumberBelow } from "./utils/numberUtils";
