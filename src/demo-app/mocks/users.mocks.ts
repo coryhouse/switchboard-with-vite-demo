f626721e8d4993ef4a52c1d58c7b3d18ef5ae033
Cory House <housecor@gmail.com>
2022-07-26 05:15:47 -0700

    Switch to array

--- src/demo-app/mocks/users.mocks.ts
+++ src/demo-app/mocks/users.mocks.ts
@@ -1,54 +1,54 @@
 import { MockUser } from "../types";
 
-export const newUser: MockUser = {
-  id: 1,
-  description: "A new user that has no todos yet.",
-  name: "Cory",
-  todos: [],
-};
-
-export const tom: MockUser = {
-  id: 1,
-  description: "A user with a few todos.",
-  name: "Tom",
-  todos: [
-    {
-      id: 1,
-      todo: "Eat chili",
-      completed: false,
-    },
-  ],
-};
-
-export const elon: MockUser = {
-  id: 1,
-  description: "A power user with many todos.",
-  name: "Elon",
-  todos: [
-    {
-      id: 1,
-      todo: "Ship Model S",
-      completed: true,
-    },
-    {
-      id: 2,
-      todo: "Ship Model 3",
-      completed: true,
-    },
-    {
-      id: 3,
-      todo: "Ship Model X",
-      completed: true,
-    },
-    {
-      id: 4,
-      todo: "Ship Cybertruck",
-      completed: false,
-    },
-    {
-      id: 5,
-      todo: "Ship Semi",
-      completed: false,
-    },
-  ],
-};
+export const mockUsers: MockUser[] = [
+  {
+    id: 1,
+    description: "A new user that has no todos yet.",
+    name: "Cory",
+    todos: [],
+  },
+  {
+    id: 1,
+    description: "A user with a few todos.",
+    name: "Tom",
+    todos: [
+      {
+        id: 1,
+        todo: "Eat chili",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    description: "A power user with many todos.",
+    name: "Elon",
+    todos: [
+      {
+        id: 1,
+        todo: "Ship Model S",
+        completed: true,
+      },
+      {
+        id: 2,
+        todo: "Ship Model 3",
+        completed: true,
+      },
+      {
+        id: 3,
+        todo: "Ship Model X",
+        completed: true,
+      },
+      {
+        id: 4,
+        todo: "Ship Cybertruck",
+        completed: false,
+      },
+      {
+        id: 5,
+        todo: "Ship Semi",
+        completed: false,
+      },
+    ],
+  },
+];
