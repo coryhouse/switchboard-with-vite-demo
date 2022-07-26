880d34defd067a301c1f9ebf05c3eaaae0fe972b
Cory House <housecor@gmail.com>
2022-07-26 06:33:42 -0700

    Show loading

--- src/demo-app/App.tsx
+++ src/demo-app/App.tsx
@@ -19,6 +19,7 @@ export default function App({ user }: AppProps) {
 
   useEffect(() => {
     async function fetchTodos() {
+      setStatus("loading");
       const todosResp = await getTodos(user.id);
       setTodos(todosResp);
       setStatus("idle");
@@ -40,47 +41,54 @@ export default function App({ user }: AppProps) {
     }
   }
 
-  if (status === "loading") return <p>Loading...</p>;
   if (submitError) throw submitError;
 
   return (
     <React.StrictMode>
       <main className="grid h-screen place-content-center">
-        <h1 className="text-3xl pb-4">Hi {user.name} 👋</h1>
-
-        {todos.length === 0 && <p>Welcome! Start entering your todos below.</p>}
-
-        <form onSubmit={onSubmit}>
-          <Input
-            id="todo"
-            label="What do you need to do?"
-            type="text"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <Button
-            type="submit"
-            className={cx("ml-1", { "bg-slate-300": status === "adding" })}
-          >
-            Add{status === "adding" && "ing..."}
-          </Button>
-        </form>
-
-        {todos.length > 0 && (
+        {status === "loading" ? (
+          <p>Loading...</p>
+        ) : (
           <>
-            <h2 className="text-2xl pt-4">Stuff to do</h2>
-            <ul>
-              {todos.map((t) => (
-                <li key={t.id}>
-                  <input
-                    type="checkbox"
-                    className="mr-1"
-                    onChange={() => markTodoComplete(t.id)}
-                  />
-                  {t.todo}
-                </li>
-              ))}
-            </ul>
+            <h1 className="text-3xl pb-4">Hi {user.name} 👋</h1>
+
+            {todos.length === 0 && (
+              <p>Welcome! Start entering your todos below.</p>
+            )}
+
+            <form onSubmit={onSubmit}>
+              <Input
+                id="todo"
+                label="What do you need to do?"
+                type="text"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+              />
+              <Button
+                type="submit"
+                className={cx("ml-1", { "bg-slate-300": status === "adding" })}
+              >
+                Add{status === "adding" && "ing..."}
+              </Button>
+            </form>
+
+            {todos.length > 0 && (
+              <>
+                <h2 className="text-2xl pt-4">Stuff to do</h2>
+                <ul>
+                  {todos.map((t) => (
+                    <li key={t.id}>
+                      <input
+                        type="checkbox"
+                        className="mr-1"
+                        onChange={() => markTodoComplete(t.id)}
+                      />
+                      {t.todo}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
           </>
         )}
       </main>
