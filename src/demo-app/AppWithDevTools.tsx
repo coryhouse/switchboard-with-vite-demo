cbcd0163dbb2450dcaa33f6512027b9d4ee7f923
Cory House <housecor@gmail.com>
2022-07-26 07:19:57 -0700

    Add http handling

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -7,35 +7,45 @@ import { useState } from "react";
 import Select from "./Select";
 import { mockUsers } from "./mocks/users.mocks";
 
-const devToolsDefaults: DevToolsConfig = {
+const devToolsConfigDefaults: DevToolsConfig = {
   user: mockUsers[1],
   apiResponse: {
-    addTodo: "success",
-    getTodos: "many",
-    markTodoCompleted: "success",
+    addTodo: {
+      delay: 0,
+      status: 200,
+    },
+    getTodos: {
+      delay: 0,
+      status: 200,
+    },
+    markTodoCompleted: {
+      delay: 0,
+      status: 200,
+    },
   },
-  httpDelay: 0,
+  delay: 0,
 };
 
 export default function AppWithDevTools() {
-  const [delay, setDelay] = useState(0);
-  const [user, setUser] = useState<MockUser>(devToolsDefaults.user);
-  const isReady = useWorker({ ...devToolsDefaults, httpDelay: delay, user });
+  const [devToolsConfig, setDevToolsConfig] = useState(devToolsConfigDefaults);
+  const isReady = useWorker(devToolsConfig);
 
-  return isReady ? (
+  if (!isReady) return <p>Initializing Mock Service Worker...</p>;
+
+  return (
     <>
-      <App user={user} />
+      <App user={devToolsConfig.user} />
       <DevTools>
         <div className="mt-4">
           <Select
             id="user"
             label="User"
-            value={user.id}
+            value={devToolsConfig.user.id}
             onChange={(e) => {
               const newUser = mockUsers.find(
                 (u) => u.id === parseInt(e.target.value)
               ) as MockUser;
-              setUser(newUser);
+              setDevToolsConfig({ ...devToolsConfig, user: newUser });
             }}
           >
             {mockUsers.map((u) => (
@@ -49,14 +59,60 @@ export default function AppWithDevTools() {
         <div className="mt-4">
           <Input
             type="number"
-            label="HTTP Delay (ms)"
-            value={delay}
-            onChange={(e) => setDelay(parseInt(e.target.value))}
+            label="Global Delay (ms)"
+            value={devToolsConfig.delay}
+            onChange={(e) =>
+              setDevToolsConfig({
+                ...devToolsConfig,
+                delay: parseInt(e.target.value),
+              })
+            }
           />
         </div>
+
+        <h2 className="mt-4">HTTP responses</h2>
+
+        <fieldset className="mt-4 border p-4">
+          <legend>getTodos</legend>
+          <div>
+            <Input
+              type="number"
+              label="Delay (ms)"
+              value={devToolsConfig.apiResponse.getTodos.delay}
+              onChange={(e) => {
+                setDevToolsConfig({
+                  ...devToolsConfig,
+                  apiResponse: {
+                    ...devToolsConfig.apiResponse,
+                    getTodos: {
+                      ...devToolsConfig.apiResponse.getTodos,
+                      delay: parseInt(e.target.value),
+                    },
+                  },
+                });
+              }}
+            />
+
+            <Input
+              type="number"
+              label="Status Code"
+              value={devToolsConfig.apiResponse.getTodos.status}
+              onChange={(e) => {
+                setDevToolsConfig({
+                  ...devToolsConfig,
+                  apiResponse: {
+                    ...devToolsConfig.apiResponse,
+                    getTodos: {
+                      ...devToolsConfig.apiResponse.getTodos,
+                      status: parseInt(e.target.value),
+                    },
+                  },
+                });
+              }}
+            />
+          </div>
+        </fieldset>
       </DevTools>
     </>
-  ) : (
-    <p>Initializing Mock Service Worker...</p>
   );
 }
