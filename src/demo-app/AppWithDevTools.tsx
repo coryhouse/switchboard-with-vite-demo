12f74d8f43c50a9e7643e1364f53a9b6624022bf
Cory House <housecor@gmail.com>
2022-07-26 05:17:26 -0700

    Read mock user

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -1,33 +1,26 @@
 import App from "./App";
 import DevTools from "../DevTools";
 import { useWorker } from "../useWorker";
-import { DevToolsConfig } from "./types";
+import { DevToolsConfig, MockUser } from "./types";
 import Input from "./Input";
 import { useState } from "react";
+import Select from "./Select";
+import { mockUsers } from "./mocks/users.mocks";
 
-const devToolsConfigDefaults: DevToolsConfig = {
+const devToolsDefaults: DevToolsConfig = {
+  user: mockUsers[0],
   apiResponse: {
     addTodo: "success",
     getTodos: "many",
     markTodoCompleted: "success",
   },
   httpDelay: 0,
-  user: {
-    id: 1,
-    name: "Cory",
-    todos: [
-      {
-        id: 1,
-        todo: "Eat chili",
-        completed: false,
-      },
-    ],
-  },
 };
 
 export default function AppWithDevTools() {
   const [delay, setDelay] = useState(0);
-  const isReady = useWorker({ ...devToolsConfigDefaults, httpDelay: delay });
+  const [user, setUser] = useState<MockUser>(devToolsDefaults.user);
+  const isReady = useWorker({ ...devToolsDefaults, httpDelay: delay });
 
   return isReady ? (
     <>
@@ -40,6 +33,16 @@ export default function AppWithDevTools() {
           onChange={(e) => setDelay(parseInt(e.target.value))}
         />{" "}
         ms
+        <Select
+          id="user"
+          label="User"
+          value={user.id}
+          onChange={(e) => setUser(parseInt(e.target.value))}
+        >
+          <option value={1}>Cory (New)</option>
+          <option value={2}>Tammy (A few todos)</option>
+          <option value={3}>Bob (Many todos)</option>
+        </Select>
       </DevTools>
     </>
   ) : (
