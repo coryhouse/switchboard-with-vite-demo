0b85618d87e78bd446f93f0e9b7d9517a11fd930
Cory House <housecor@gmail.com>
2022-07-26 21:26:07 -0700

    Markup fix

--- src/CloseButton.tsx
+++ src/CloseButton.tsx
@@ -13,11 +13,11 @@ export default function CloseButton(props: ButtonProps) {
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M6 18L18 6M6 6l12 12"
         />
       </svg>
