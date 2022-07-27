b7bea9db98f069976cdd938eb1537092ad75467c
Cory House <housecor@gmail.com>
2022-07-27 06:28:10 -0700

    Fix classname

--- src/demo-app/Input.tsx
+++ src/demo-app/Input.tsx
@@ -1,17 +1,22 @@
+import cx from "clsx";
+
 interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
   /** Input label */
   label: string;
 }
 
 export default function Input(props: InputProps) {
-  const { id, onChange, value, ...rest } = props;
+  const { id, onChange, value, className, ...rest } = props;
   return (
     <>
       <label className="block" htmlFor={id}>
         {props.label}
       </label>
       <input
-        className="border-slate-400 border-solid border p-1 rounded"
+        className={cx(
+          "border-slate-400 border-solid border p-1 rounded",
+          className
+        )}
         type="text"
         id={id}
         value={value}
