57da64fda173432b53bd1ae03e0f2fd3cce9d0b8
Cory House <housecor@gmail.com>
2022-07-26 20:39:30 -0700

    Add

---
+++ src/demo-app/CloseButton.tsx
@@ -0,0 +1,26 @@
+import Button, { ButtonProps } from "./Button";
+
+export default function CloseButton(props: ButtonProps) {
+  return (
+    <Button
+      {...props}
+      className="bg-white border-none p-1 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100"
+    >
+      <span className="sr-only">Close menu</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </Button>
+  );
+}
