66a8970062c2d13b9781546ba8cb95a968e342ec
Cory House <housecor@gmail.com>
2022-07-30 20:40:58 -0700

    Refactor

--- src/demo-app/mocks/users.mocks.ts
+++ src/demo-app/mocks/users.mocks.ts
@@ -1,54 +1,43 @@
 import { MockUser } from "../types";
 
-export const mockUsers: MockUser[] = [
-  {
-    id: 1,
-    description: "No todos",
-    name: "Cory",
-    todos: [],
-  },
-  {
-    id: 2,
-    description: "Some todos",
-    name: "Tom",
-    todos: [
-      {
-        id: 1,
-        todo: "Eat chili",
-        completed: false,
-      },
-    ],
-  },
-  {
-    id: 3,
-    description: "Many todos",
-    name: "Elon",
-    todos: [
-      {
-        id: 1,
-        todo: "Ship Model S",
-        completed: true,
-      },
-      {
-        id: 2,
-        todo: "Ship Model 3",
-        completed: true,
-      },
-      {
-        id: 3,
-        todo: "Ship Model X",
-        completed: true,
-      },
-      {
-        id: 4,
-        todo: "Ship Cybertruck",
-        completed: false,
-      },
-      {
-        id: 5,
-        todo: "Ship Semi",
-        completed: false,
-      },
-    ],
-  },
-];
+export const noTodos: MockUser = {
+  id: 1,
+  description: "No todos",
+  name: "Cory",
+  todos: [],
+};
+
+export const manyTodos: MockUser = {
+  id: 3,
+  description: "Many todos",
+  name: "Elon",
+  todos: [
+    {
+      id: 1,
+      todo: "Ship Model S",
+      completed: true,
+    },
+    {
+      id: 2,
+      todo: "Ship Model 3",
+      completed: true,
+    },
+    {
+      id: 3,
+      todo: "Ship Model X",
+      completed: true,
+    },
+    {
+      id: 4,
+      todo: "Ship Cybertruck",
+      completed: false,
+    },
+    {
+      id: 5,
+      todo: "Ship Semi",
+      completed: false,
+    },
+  ],
+};
+
+export const mockUsers: MockUser[] = [noTodos, manyTodos];
