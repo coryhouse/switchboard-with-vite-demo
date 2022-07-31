3c059518cd3423fcf84db4fad478009ca0fd4f23
Cory House <housecor@gmail.com>
2022-07-31 18:24:18 -0500

    Add

--- cypress/e2e/todo-app.cy.ts
+++ cypress/e2e/todo-app.cy.ts
@@ -7,8 +7,14 @@ function isInSection(heading: string, text: string) {
     });
 }
 
-describe("Todo App", () => {
-  it("shows existing todos on initial load, supports adding a todo, and marking as complete", () => {
+// Returns a URL with the provided devtools config included in the querystring.
+function buildUrl(config: Partial<DevToolsConfig>) {
+  const params = new URLSearchParams(location.search);
+  Object.keys(config).forEach((key) => {
+    params.append(key, encodeURI(config[key]));
+  });
+  return "http://127.0.0.1:5173/?" + params.toString();
+}
     // Visit Elon with 50ms delay on getTodos
     cy.visit(
       "http://127.0.0.1:5173/?devtools=%257B%2522user%2522%3A%257B%2522id%2522%3A3%2C%2522description%2522%3A%2522Many%2520todos%2522%2C%2522name%2522%3A%2522Elon%2522%2C%2522todos%2522%3A%255B%257B%2522id%2522%3A1%2C%2522todo%2522%3A%2522Ship%2520Model%2520S%2522%2C%2522completed%2522%3Atrue%257D%2C%257B%2522id%2522%3A2%2C%2522todo%2522%3A%2522Ship%2520Model%25203%2522%2C%2522completed%2522%3Atrue%257D%2C%257B%2522id%2522%3A3%2C%2522todo%2522%3A%2522Ship%2520Model%2520X%2522%2C%2522completed%2522%3Atrue%257D%2C%257B%2522id%2522%3A4%2C%2522todo%2522%3A%2522Ship%2520Cybertruck%2522%2C%2522completed%2522%3Afalse%257D%2C%257B%2522id%2522%3A5%2C%2522todo%2522%3A%2522Ship%2520Semi%2522%2C%2522completed%2522%3Afalse%257D%255D%257D%2C%2522autoReload%2522%3Atrue%2C%2522delay%2522%3A50%2C%2522mockApis%2522%3A%255B%257B%2522label%2522%3A%2522addTodo%2522%2C%2522delay%2522%3A0%2C%2522status%2522%3A200%257D%2C%257B%2522label%2522%3A%2522getTodos%2522%2C%2522delay%2522%3A0%2C%2522status%2522%3A200%257D%2C%257B%2522label%2522%3A%2522markTodoCompleted%2522%2C%2522delay%2522%3A0%2C%2522status%2522%3A200%257D%255D%257D"
