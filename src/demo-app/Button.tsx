f68a3140cc1b88994b5296495732f5537421eca9
Cory House <housecor@gmail.com>
2022-07-31 20:55:35 -0500

    Simplify style

--- src/demo-app/Button.tsx
+++ src/demo-app/Button.tsx
@@ -4,10 +4,7 @@ export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 export default function Button({ className, ...rest }: ButtonProps) {
   return (
     <button
-      className={cx(
-        className,
-        "border border-slate-400 p-1 bg-blue-600 rounded text-white"
-      )}
+      className={cx(className, "border border-slate-400 p-1 rounded")}
       {...rest}
     />
   );
