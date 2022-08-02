3207b11705ae74ff47e83306e26bbb2ccbfb25b7
Cory House <housecor@gmail.com>
2022-08-02 16:37:18 -0500

    Remove unused func

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,31 +1,9 @@
-import { DevToolsConfig } from "../demo-app/types";
-
-/** Return the devtools entry from the provided URL's querystring as an object */
-export function getDevToolsSettingsFromUrlQuerystring(
-  url: string
-): DevToolsConfig | null {
-  const urlParams = new URLSearchParams(url);
-  const devToolsQuery = urlParams.get("devtools");
-  if (!devToolsQuery) return null;
-  try {
-    const settings = JSON.parse(
-      decodeURIComponent(devToolsQuery)
-    ) as DevToolsConfig;
-    return settings;
-  } catch (err) {
-    console.error(err);
-    throw new Error(
-      "Parsing the querystring's devtools setting failed. The querystring is likely malformed."
-    );
-  }
-}
-
 /** Returns a string that contains the current URL with DevTools settings included in the querystring */
-export function getDevToolsUrl(url: URL, config: any) {
+export function getDevToolsUrl(url: URL, key: string, config: any) {
   const urlWithoutQuerystring = url.href.split("?")[0];
   const params = new URLSearchParams(url.search);
   // Remove existing querystring if it exists. This assures the newly generated URL only contains the devtools querystring once.
-  params.delete("devtools");
-  params.append("devtools", JSON.stringify(config));
+  params.delete(key);
+  params.append(key, JSON.stringify(config));
   return urlWithoutQuerystring + "?" + params.toString();
 }
