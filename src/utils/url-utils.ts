296e8d52ea7fbff739fe063642791f8c56979745
Cory House <housecor@gmail.com>
2022-08-03 07:16:40 -0500

    Fix comment

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -2,7 +2,10 @@
 export function getDevToolsUrl(url: URL, key: string, config: any = null) {
   const urlWithoutQuerystring = url.href.split("?")[0];
   const params = new URLSearchParams(url.search);
-  // Remove existing querystring if it exists. This assures the newly generated URL only contains the devtools querystring once.
+  // Remove existing querystring if it exists. Here's why:
+  // 1. This assures the newly generated URL doesn't contain the param twice.
+  // 2. We only add the param if a config value is provided,
+  // so removing it cleans up the URL if no data has been provided for the key.
   params.delete(key);
   if (config) params.append(key, JSON.stringify(config));
   return urlWithoutQuerystring + "?" + params.toString();
