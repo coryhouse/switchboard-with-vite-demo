8ddd422b173fda0e7d3dc751d2d89e78172fa2f1
Cory House <housecor@gmail.com>
2022-08-03 18:09:53 -0500

    Comment out feature

--- src/hooks/useDevToolsState.ts
+++ src/hooks/useDevToolsState.ts
@@ -95,23 +95,27 @@ export function useDevToolsState<T>(
       // Step 1: Save state, so React re-renders
       setStoredValue(valueToStore);
 
+      // TODO: Reenable as an optional feature.
+      // Disabling for now since it likely isn't a good default behavior anyway because it muddies the URL at all times for the small benefit of sharing settings without needing to click a button.
+      // And actually, clicking the button to add to the clipboard is actually a single click, so plenty easy.
+
       // Step 2: Update the URL so it reflects the new setting, and can thus be copied and shared with others
       // If the value matches the default value, then remove it from the URL (to keep the URL as lean as possible).
       // However, go ahead and put the value in the URL anyway if showDefaultValuesInTheUrl is true.
-      if (valueToStore == defaultValue && !options?.showDefaultValuesInTheUrl) {
-        const newUrl = getUrlWithUpdatedQuery(
-          new URL(window.location.href),
-          key
-        );
-        window.history.pushState("", "DevTools state update", newUrl);
-      } else {
-        const newUrl = getUrlWithUpdatedQuery(
-          new URL(window.location.href),
-          key,
-          valueToStore
-        );
-        window.history.pushState("", "DevTools state update", newUrl);
-      }
+      // if (valueToStore == defaultValue && !options?.showDefaultValuesInTheUrl) {
+      //   const newUrl = getUrlWithUpdatedQuery(
+      //     new URL(window.location.href),
+      //     key
+      //   );
+      //   window.history.pushState("", "DevTools state update", newUrl);
+      // } else {
+      //   const newUrl = getUrlWithUpdatedQuery(
+      //     new URL(window.location.href),
+      //     key,
+      //     valueToStore
+      //   );
+      //   window.history.pushState("", "DevTools state update", newUrl);
+      // }
 
       // Step 3: Save to local storage, so the settings persist after the window is closed
       if (typeof window !== "undefined") {
