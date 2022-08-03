7889dc8cf3301df9cbec098f22448a54b5632654
Cory House <housecor@gmail.com>
2022-08-03 16:00:23 -0500

    Update path

--- src/demo-app/ErrorFallback.tsx
+++ src/demo-app/ErrorFallback.tsx
@@ -1,5 +1,5 @@
 import { FallbackProps } from "react-error-boundary";
-import Button from "./Button";
+import Button from "../components/Button";
 
 export default function ErrorFallback({
   error,
