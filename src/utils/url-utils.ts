f7a98d5a3f07ef54a46fd8096275b175df738341
Cory House <housecor@gmail.com>
2022-08-02 22:24:55 -0500

    Make config arg optional

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -1,9 +1,9 @@
 /** Returns a string that contains the current URL with DevTools settings included in the querystring */
-export function getDevToolsUrl(url: URL, key: string, config: any) {
+export function getDevToolsUrl(url: URL, key: string, config: any = null) {
   const urlWithoutQuerystring = url.href.split("?")[0];
   const params = new URLSearchParams(url.search);
   // Remove existing querystring if it exists. This assures the newly generated URL only contains the devtools querystring once.
   params.delete(key);
-  params.append(key, JSON.stringify(config));
+  if (config) params.append(key, JSON.stringify(config));
   return urlWithoutQuerystring + "?" + params.toString();
 }
