2a467f7da97c78604b0d4341b3ccb30467cedec0
Cory House <housecor@gmail.com>
2022-08-03 13:39:15 -0500

    Add option to select user

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -58,6 +58,7 @@ export default function AppWithDevTools() {
               value={userId}
               onChange={(e) => setUserId(parseInt(e.target.value))}
             >
+              <option value="">Select User</option>
               {mockUsers.map((u) => (
                 <option value={u.id} key={u.id}>
                   {u.name} ({u.description.role}, {u.description.todos})
