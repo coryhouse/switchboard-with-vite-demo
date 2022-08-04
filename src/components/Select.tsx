cc4f664a756f39e4fda4f69fcbef05ec98bf1e8a
Cory House <housecor@gmail.com>
2022-08-03 21:24:31 -0500

    Support changed on select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Label from "./Label";
 
 interface SelectProps extends React.ComponentPropsWithoutRef<"select"> {
@@ -12,11 +13,13 @@ export default function Select(props: SelectProps) {
   const { id, onChange, changed = false, label, value, ...rest } = props;
   return (
     <>
-      <Label className="block" htmlFor={id} changed={changed}>
+      <Label className="block" htmlFor={id}>
         {label}
       </Label>
       <select
-        className="border-slate-400 border-solid border p-1 rounded"
+        className={clsx("border-slate-400 border-solid border p-1 rounded", {
+          "bg-yellow-100": changed,
+        })}
         id={id}
         value={value}
         onChange={onChange}
