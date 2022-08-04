01ec63b326b9411674c55b45539dba29cd79f05a
Cory House <housecor@gmail.com>
2022-08-03 21:06:23 -0500

    Revert

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,22 +1,8 @@
-import cx from "clsx";
-
 type FieldProps = {
   /** Child elements */
   children: React.ReactNode;
-
-  /** Set to true to style the field visually as customized */
-  customized?: boolean;
 };
 
-export default function Field({ children, customized = false }: FieldProps) {
-  return (
-    <div
-      className={cx(
-        { "bg-yellow-100 border-red-500 border": customized },
-        "mt-4"
-      )}
-    >
-      {children}
-    </div>
-  );
+export default function Field({ children }: FieldProps) {
+  return <div className="mt-4">{children}</div>;
 }
