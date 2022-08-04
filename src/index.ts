2eb50ca8565f1a8b7d35e50dadbd3a9970f28409
Cory House <housecor@gmail.com>
2022-08-04 09:43:26 -0500

    Update entry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// The items exported from this file will be included in the npm package
 import * as DevTools from "./DevTools";
+import * as useWorker from "./hooks/useWorker";
+import * as useDevToolsState from "./hooks/useDevToolsState";
 
-export { DevTools };
+export { DevTools, useWorker, useDevToolsState };
