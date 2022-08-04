f5bfcb56276034dbc419983f545e878fb2607ab4
Cory House <housecor@gmail.com>
2022-08-03 20:43:49 -0500

    Add changed support

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,15 +1,20 @@
+import Label from "./Label";
+
 interface SelectProps extends React.ComponentPropsWithoutRef<"select"> {
+  /** Set to true to highlight the label so that it is visually marked as changed from the default. */
+  changed?: boolean;
+
   /** Input label */
   label: string;
 }
 
 export default function Select(props: SelectProps) {
-  const { id, onChange, value, ...rest } = props;
+  const { id, onChange, changed, label, value, ...rest } = props;
   return (
     <>
-      <label className="block" htmlFor={id}>
-        {props.label}
-      </label>
+      <Label className="block" htmlFor={id} changed={changed}>
+        {label}
+      </Label>
       <select
         className="border-slate-400 border-solid border p-1 rounded"
         id={id}
