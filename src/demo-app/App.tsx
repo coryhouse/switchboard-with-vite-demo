b2d66e67ea6727a04f1d25090100cb3b91e4f0f3
Cory House <housecor@gmail.com>
2022-08-04 21:07:11 -0500

    Simplify app

--- src/demo-app/App.tsx
+++ src/demo-app/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { UserContextProvider } from "./contexts/UserContext";
 import Login from "./Login";
 import Todos from "./Todos";
-import { User } from "./types";
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
   return (
-    <UserContextProvider user={user} setUser={setUser}>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/todos" element={<Todos />} />
-      </Routes>
-    </UserContextProvider>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/todos" element={<Todos />} />
+    </Routes>
   );
 }
