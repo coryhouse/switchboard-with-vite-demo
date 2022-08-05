4efdf3a2a1ca2463df4c54bb61a0e489210f368f
Cory House <housecor@gmail.com>
2022-08-05 07:13:16 -0500

    Remove unused import

--- src/DevTools.tsx
+++ src/DevTools.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, ChangeEvent } from "react";
+import React, { useState, useRef } from "react";
 import Button from "./components/Button";
 import cx from "clsx";
 import CloseButton from "./components/CloseButton";
