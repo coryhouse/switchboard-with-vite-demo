53c2936750453e1fafeff2f5364111137d5f70ae
Cory House <housecor@gmail.com>
2022-08-04 20:47:36 -0500

    Wrap in context

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import "./index.css";
 import ErrorFallback from "./demo-app/ErrorFallback";
 import { BrowserRouter } from "react-router-dom";
+import { UserContextProvider } from "./demo-app/contexts/UserContext";
 
 // Lazy load so it's not part of the prod bundle.
 const AppWithDevTools = React.lazy(
@@ -24,15 +25,17 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <BrowserRouter>
-        {useDevTools ? (
-          <Suspense fallback="Loading with devtools...">
-            <AppWithDevTools />
-          </Suspense>
-        ) : (
-          <App />
-        )}
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          {useDevTools ? (
+            <Suspense fallback="Loading with devtools...">
+              <AppWithDevTools />
+            </Suspense>
+          ) : (
+            <App />
+          )}
+        </BrowserRouter>
+      </UserContextProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
