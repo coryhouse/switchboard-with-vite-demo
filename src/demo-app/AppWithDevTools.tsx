d6a19318d9d32d490de0eb2dac571e58205272b2
Cory House <housecor@gmail.com>
2022-08-05 21:34:14 -0500

    Move code to devTools

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -1,32 +1,19 @@
 import App from "./App";
 import DevTools from "../DevTools";
-import { useWorker } from "../hooks/useWorker";
-import { Endpoint, endpoints, HttpSetting } from "./types";
-import Input from "../components/Input";
 import Select from "../components/Select";
-import { mockUsers } from "./mocks/users.mocks";
+import { personas } from "./mocks/personas";
 import { useDevToolsState } from "../hooks/useDevToolsState";
-import { ErrorBoundary } from "react-error-boundary";
-import HttpSettingForm from "./HttpSettingForm";
-import ErrorFallback from "./ErrorFallback";
 import Field from "../components/Field";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { requestHandlers } from "./mocks/request-handlers";
 import { useUserContext } from "./contexts/UserContext";
-
-export const httpDefaults = {
-  delay: 0,
-  status: 200,
-  response: "",
-};
+import { HttpSettings } from "../types/types";
+import { generateRequestHandlers } from "./mocks/request-handlers";
 
 export default function AppWithDevTools() {
   // Storing only userId in devToolsState to keep localStorage and URL minimal.
-  // Storing the entire user would simplify the UserContext integration (since it stores the full user), but would bloat localStorage and the URL.
+  // Storing the entire persona would bloat localStorage and the URL.
   const [userId, setUserId] = useDevToolsState<number | "">("userId", "");
-  const [delay, setDelay, delayChanged] = useDevToolsState("delay", 0);
-  const [http, setHttp] = useDevToolsState<HttpSetting[]>("http", []);
 
   const { user } = useUserContext();
 
@@ -42,28 +29,9 @@ export default function AppWithDevTools() {
 
   const navigate = useNavigate();
 
-  const devToolsConfig = {
-    userId,
-    delay,
-    http,
-  };
-
-  const isReady = useWorker(devToolsConfig, requestHandlers, {
-    onUnhandledRequest: ({ method, url }) => {
-      // Ignore these requests that need not be mocked
-      if (
-        url.pathname !== "/src/demo-app/CloseButton.tsx" &&
-        url.pathname !== "/src/index.css" &&
-        !url.pathname.startsWith("chrome-extension:")
-      ) {
-        throw new Error(`Unhandled ${method} request to ${url}`);
-      }
-    },
-  });
-
   function simulateLogin(userId: number) {
     setUserId(userId);
-    const user = mockUsers.find((u) => u.id === userId);
+    const user = personas.find((u) => u.id === userId);
     if (!user) throw new Error("Can't find user: " + userId);
     localStorage.setItem("userId", JSON.stringify(userId));
     navigate("/todos");
@@ -75,94 +43,61 @@ export default function AppWithDevTools() {
     navigate("/");
   }
 
-  if (!isReady) return <p>Initializing...</p>;
+  const httpSettings: HttpSettings = {
+    endpoints: [
+      "login",
+      "getUser",
+      "getTodos",
+      "addTodo",
+      "deleteTodo",
+      "toggleTodoCompleted",
+    ],
+    generateRequestHandlers,
+    startOptions: {
+      onUnhandledRequest: ({ method, url }) => {
+        // Ignore these requests that need not be mocked
+        if (
+          url.pathname !== "/src/demo-app/CloseButton.tsx" &&
+          url.pathname !== "/src/index.css" &&
+          !url.pathname.startsWith("chrome-extension:")
+        ) {
+          throw new Error(`Unhandled ${method} request to ${url}`);
+        }
+      },
+    },
+  };
 
   return (
-    <>
-      {/* Wrap app in ErrorBoundary so DevTools continue to display upon error */}
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        {/* Passing a key to force the app to completely reinitialize when the userId changes. */}
-        <App key={userId} />
-      </ErrorBoundary>
-
-      <DevTools
-        defaultPosition="top-left"
-        openByDefault
-        closeViaEscapeKey
-        devToolsConfig={devToolsConfig}
-      >
-        <>
-          <Field>
-            <Select
-              width="full"
-              id="user"
-            label="Persona"
-              value={userId}
-              onChange={(e) => {
-                const userId = parseInt(e.target.value);
-                userId ? simulateLogin(userId) : simulateLogout();
-              }}
-            >
-              <option value="">Logged out</option>
-              {mockUsers.map((u) => (
-                <option value={u.id} key={u.id}>
-                  {u.name} ({u.description.role}, {u.description.todos})
-                </option>
-              ))}
-            </Select>
-          </Field>
-
-          <details open>
-            <summary className="mt-4 font-bold">HTTP</summary>
-            <Field>
-              <Input
-                width="full"
-                changed={delayChanged}
-                type="number"
-                label="Global Delay (ms)"
-                value={delay}
-                onChange={(e) => setDelay(parseInt(e.target.value))}
-              />
-            </Field>
-
-            <Field>
-              <Select
-                width="full"
-                label="Customize Endpoint"
-                // Value need not change since the selected value disappears once selected.
-                value=""
-                onChange={(e) => {
-                  setHttp([
-                    ...http,
-                    {
-                      endpoint: e.target.value as Endpoint,
-                      delay: httpDefaults.delay,
-                      status: httpDefaults.status,
-                      response: httpDefaults.response,
-                    },
-                  ]);
-                }}
-              >
-                <option>Select Endpoint</option>
-                {endpoints
-                  // Filter out endpoints that are already customized
-                  .filter((e) => !http.some((h) => h.endpoint === e))
-                  .map((e) => (
-                    <option key={e}>{e}</option>
-                  ))}
-              </Select>
-            </Field>
-
-            {http.map((setting) => (
-              <HttpSettingForm
-                key={setting.endpoint}
-                http={setting}
-                setHttp={setHttp}
-              />
-            ))}
-          </details>
-        </>
-      </DevTools>
-    </>
+    <DevTools
+      defaultPosition="top-left"
+      openByDefault
+      closeViaEscapeKey
+      httpSettings={httpSettings}
+      // Passing a key to force the app to reinitialize when the userId changes.
+      appSlot={<App key={userId} />}
+      customSettings={{
+        userId,
+      }}
+    >
+      <Field>
+        <Select
+          width="full"
+          id="user"
+          label="Persona"
+          value={userId}
+          onChange={(e) => {
+            const userId = parseInt(e.target.value);
+            userId ? simulateLogin(userId) : simulateLogout();
+          }}
+        >
+          <option value="">Logged out</option>
+          {personas.map((u) => (
+            <option value={u.id} key={u.id}>
+              {u.name} ({u.description.role}, {u.description.todos})
+            </option>
+          ))}
+        </Select>
+      </Field>
+    </DevTools>
   );
 }
