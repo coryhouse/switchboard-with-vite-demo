94f7af38e15cf040307cc46fb51a665e8853d1da
Cory House <housecor@gmail.com>
2022-08-07 12:28:55 -0500

    Add docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+// TODO: Create separate config for docs and lib
+// Uncomment these lines to do the docs build instead of a lib build.
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
