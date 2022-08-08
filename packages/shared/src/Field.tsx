8a453902ca4f9a4e14f03810e1084dacf4b22f9a
Cory House <housecor@gmail.com>
2022-08-08 16:35:27 -0500

    Move for now

--- packages/shared/src/Field.tsx
+++
@@ -1,8 +0,0 @@
-type FieldProps = {
-  /** Child elements */
-  children: React.ReactNode;
-};
-
-export default function Field({ children }: FieldProps) {
-  return <div className="mt-4">{children}</div>;
-}
