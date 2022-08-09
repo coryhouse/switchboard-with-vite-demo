2b7474bf820e164ab3697906db2c595dbed8e47d
Cory House <housecor@gmail.com>
2022-08-09 00:14:07 -0500

    add config

--- packages/react-switchboard/vite.config.ts
+++ packages/react-switchboard/vite.config.ts
@@ -7,13 +7,17 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    dts({
+      insertTypesEntry: true,
+    }),
     visualizer({
-      open: true,
+      open: false,
       template: "sunburst",
     }),
   ],
