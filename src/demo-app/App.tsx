6d838616b6043f4245d3234bf7d515a2197eca51
Cory House <housecor@gmail.com>
2022-09-05 15:14:47 -0500

    Consume protected route

--- src/demo-app/App.tsx
+++ src/demo-app/App.tsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import ProtectedRoute from "./ProtectedRoute";
 import Todos from "./Todos";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/todos" element={<Todos />} />
+
+      <Route
+        path="/todos"
+        element={
+          <ProtectedRoute>
+            <Todos />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
