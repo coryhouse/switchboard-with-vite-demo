9210c3a4e3f056939818fdf139f207c19aa8008a
Cory House <housecor@gmail.com>
2023-01-17 19:12:35 -0600

    Refactor

--- src/hooks/useWorker.ts
+++ src/hooks/useWorker.ts
@@ -7,17 +7,8 @@ export const useWorker = <TCustomSettings>(
   { startOptions, requestHandlers }: HttpSettings,
   config: TCustomSettings
 ) => {
-  const configRef = useRef(config);
   const workerRef = useRef(setupWorker(...requestHandlers(config)));
   const [isReady, setIsReady] = useState(false);
-
-  // Store the config in a ref so the useEffect below that starts
-  // the worker runs only once, yet reads the latest config values
-  // as they change in the devtools.
-  useEffect(() => {
-    configRef.current = config;
-  }, [config]);
-
   // Store the previous config in a ref so we can compare it to the current config
   // This way, we know if the config has changed.
   const prevConfig = useRef<TCustomSettings | undefined>(undefined);
@@ -28,15 +19,12 @@ export const useWorker = <TCustomSettings>(
     });
   }
 
-  useEffect(
-    function updateWorker() {
-      // This only needs to re-run if the config has changed so the new settings apply.
-      if (prevConfig.current === config) return;
-      prevConfig.current === config;
-      workerRef.current.use(...requestHandlers(config));
-    },
-    [config, requestHandlers, startOptions]
-  );
+  useEffect(() => {
+    // This only needs to re-run if the config has changed so the new settings apply.
+    if (prevConfig.current === config) return;
+    prevConfig.current === config;
+    workerRef.current.use(...requestHandlers(config));
+  }, [config, requestHandlers, startOptions]);
 
   return isReady;
 };
