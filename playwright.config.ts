4d84f15bb91d91b9e3886e0964e90ff5b8cefa3f
Cory House <housecor@gmail.com>
2024-07-22 07:16:42 -0500

    3000

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,8 @@ import { defineConfig, devices } from "@playwright/test";
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const baseUrl = "http://localhost:3000/";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -25,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173/",
+    baseURL: baseUrl,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5173/",
+    url: baseUrl,
     reuseExistingServer: !process.env.CI,
   },
 });
