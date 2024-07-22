fc17f18565bbe717bf659583c50fefd30d0c81e1
Cory House <housecor@gmail.com>
2024-07-22 16:55:07 -0500

    streamline

--- src/demo-app/apis/todo-apis.ts
+++ src/demo-app/apis/todo-apis.ts
@@ -1,41 +1,23 @@
 import { Todo } from "../demo-app-types";
 
-// NOTE: For simplicity, real endpoints don't actually exist in this demo.
-// These fetch calls are intercepted by msw when mocking is enabled.
+// NOTE: For simplicity, no real endpoints exist in this demo.
+// These calls are intercepted by msw when mocking is enabled.
 // Mock Service Worker intercepts the call made in the browser
 // and returns a mock response instead.
+import ky, { KyResponse } from "ky";
+
 export async function getTodos(): Promise<Todo[]> {
-  const resp = await fetch(`/todos`);
-  if (!resp.ok) throw resp;
-  return resp.json() as Promise<Todo[]>;
+  return ky.get(`/todos`).json<Todo[]>();
 }
 
 export async function addTodo(todo: string): Promise<Todo> {
-  const resp = await fetch("/todo", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ todo }),
-  });
-  if (!resp.ok) throw resp;
-  return resp.json() as Promise<Todo>;
+  return ky.post("/todo", { json: { todo } }).json<Todo>();
 }
 
-export async function updateTodo(todo: Todo): Promise<void> {
-  const resp = await fetch(`/todo/${todo.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ todo }),
-  });
-  if (!resp.ok) throw resp;
+export async function updateTodo(todo: Todo): Promise<KyResponse> {
+  return ky.put(`/todo/${todo.id}`, { json: { todo } });
 }
 
-export async function deleteTodo(todoId: number): Promise<void> {
-  const resp = await fetch(`/todo/${todoId}`, {
-    method: "DELETE",
-  });
-  if (!resp.ok) throw resp;
+export async function deleteTodo(todoId: number): Promise<KyResponse> {
+  return ky.delete(`/todo/${todoId}`);
 }
