733950426365eabf305a69e8be3e9aa634cfc70e
Cory House <housecor@gmail.com>
2024-07-21 20:54:58 -0500

    up

--- src/demo-app/mocks/handlers/todo.handlers.ts
+++ src/demo-app/mocks/handlers/todo.handlers.ts
@@ -1,60 +1,70 @@
 import { getRandomNumberBelow } from "../../../utils/number-utils";
-import { RequestHandler, rest } from "msw";
-import { RequestHandlerConfig, Todo } from "../../demo-app-types";
+import { RequestHandler, delay, http } from "msw";
+import { RequestHandlerConfig, Todo, todoSchema } from "../../demo-app-types";
 import { getCustomResponseSettings, getDelay, getUser } from "../mock-utils";
 
 export function getTodoHandlers(
   configRef: React.MutableRefObject<RequestHandlerConfig>
 ): RequestHandler[] {
   return [
-    rest.get("/todos", async (_req, res, ctx) => {
+    http.get("/todos", async () => {
       const setting = getCustomResponseSettings(configRef, "GET /todos");
       const user = getUser(configRef);
-      if (!user) return res(ctx.status(401));
+      if (!user)
+        return new Response(null, {
+          status: 401,
+        });
 
-      return res(
-        ctx.delay(getDelay(configRef, setting?.delay)),
-        ctx.json(setting?.response ?? user.response.todos),
-        ctx.status(setting?.status ?? 200)
+      await delay(getDelay(configRef, setting?.delay));
+
+      return new Response(
+        setting?.response ?? JSON.stringify(user.response.todos),
+        {
+          status: setting?.status ?? 200,
+          headers: { "Content-Type": "application/json" },
+        }
       );
     }),
 
-    rest.post("/todo", async (req, res, ctx) => {
-      const { todo } = await req.json();
+    http.post("/todo", async ({ request }) => {
+      const { todo } = todoSchema.parse(await request.json());
       const user = getUser(configRef);
-      if (!user) return res(ctx.status(401));
+      if (!user)
+        return new Response(null, {
+          status: 401,
+        });
       const defaultResp: Todo = {
         // TODO: Read the todos from memory so the reduce call below works.
         // They're in Todos.tsx's state, so how can we access them?
         // const maxTodoId = user.todos.reduce((max, t) => Math.max(max, t.id), 0);
         id: getRandomNumberBelow(100000),
         completed: false,
-        todo: todo as string,
+        todo,
       };
+
       const setting = getCustomResponseSettings(configRef, "POST /todo");
-      return res(
-        ctx.delay(getDelay(configRef, setting?.delay)),
-        ctx.json(setting?.response ?? defaultResp),
-        ctx.status(setting?.status ?? 200)
-      );
+      await delay(getDelay(configRef, setting?.delay));
+      return new Response(setting?.response ?? JSON.stringify(defaultResp), {
+        status: setting?.status ?? 200,
+        headers: { "Content-Type": "application/json" },
+      });
     }),
 
-    rest.put("/todo/:id", async (_req, res, ctx) => {
+    http.put("/todo/:id", async () => {
       const setting = getCustomResponseSettings(configRef, "PUT /todo/:id");
-      return res(
-        ctx.delay(getDelay(configRef, setting?.delay)),
-        ctx.json(setting?.response ?? ""),
-        ctx.status(setting?.status ?? 200)
-      );
+      await delay(getDelay(configRef, setting?.delay));
+      return new Response(setting?.response ?? "", {
+        status: setting?.status ?? 200,
+      });
     }),
 
-    rest.delete("/todo/:id", async (_req, res, ctx) => {
+    http.delete("/todo/:id", async () => {
       const setting = getCustomResponseSettings(configRef, "DELETE /todo/:id");
-      return res(
-        ctx.delay(getDelay(configRef, setting?.delay)),
-        ctx.json(setting?.response ?? ""),
-        ctx.status(setting?.status ?? 200)
-      );
+      await delay(getDelay(configRef, setting?.delay));
+
+      return new Response(setting?.response ?? "", {
+        status: setting?.status ?? 200,
+      });
     }),
   ];
 }
