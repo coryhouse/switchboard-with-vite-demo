947799c124ee4647e6933e1f7b989b02e0498b2b
Cory House <housecor@gmail.com>
2024-07-23 09:21:06 -0500

    add simulate traffic

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -6,12 +6,22 @@ import { useDevToolsState } from "../hooks/useDevToolsState";
 import Field from "../components/Field";
 import { requestHandlers } from "./mocks/handlers/handlers";
 import useUserSync from "./useUserSync";
+import Button from "../components/Button";
+import { addTodo, deleteTodo } from "./apis/todo-apis";
+import Input from "../components/Input";
+import { useState } from "react";
+
+type SimulateAction = "add" | "edit" | "delete";
 
 export default function AppWithDevTools() {
   // Storing only userId in devToolsState to keep localStorage and URL minimal.
   // Storing the entire persona would bloat localStorage and the URL.
   const [userId, setUserId] = useDevToolsState<number | "">("userId", "");
   useUserSync(userId, setUserId);
+
+  const [simulateAction, setSimulateAction] = useState<SimulateAction>("add");
+  const [simulateAdd, setSimulateAdd] = useState("");
+  const [simulateDelete, setSimulateDelete] = useState("");
 
   return (
     <DevTools
@@ -58,6 +68,62 @@ export default function AppWithDevTools() {
           ))}
         </Select>
       </Field>
+
+      <details className="mt-4" open>
+        <summary className="font-bold">Simulate Traffic</summary>
+        <Field>
+          <Select
+            label="Action"
+            value={simulateAction}
+            onChange={(e) => {
+              setSimulateAction(e.target.value as SimulateAction);
+            }}
+          >
+            <option value="add">Add</option>
+            <option value="delete">Delete</option>
+            <option value="edit">Edit</option>
+          </Select>
+        </Field>
+
+        {simulateAction === "add" && (
+          <Field>
+            <Input
+              label="Todo"
+              value={simulateAdd}
+              id="simulate-add"
+              onChange={(e) => setSimulateAdd(e.target.value)}
+            />
+            <Button
+              onClick={() => {
+                addTodo(simulateAdd);
+              }}
+            >
+              Add
+            </Button>
+          </Field>
+        )}
+
+        {simulateAction === "delete" && (
+          <Field>
+            <Input
+              label="Todo Id"
+              value={simulateDelete}
+              type="number"
+              id="simulate-delete"
+              onChange={(e) => setSimulateDelete(e.target.value)}
+            />
+            <Button
+              onClick={() => {
+                if (!simulateDelete) return;
+                deleteTodo(parseInt(simulateDelete));
+                setSimulateDelete("");
+              }}
+            >
+              Delete
+            </Button>
+          </Field>
+        )}
+      </details>
     </DevTools>
   );
 }
