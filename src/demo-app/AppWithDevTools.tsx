fc5415c18df9bd57d298542f2a53082ba4914802
Cory House <housecor@gmail.com>
2024-08-06 16:27:28 -0500

    Call extracted stuff

--- src/demo-app/AppWithDevTools.tsx
+++ src/demo-app/AppWithDevTools.tsx
@@ -1,27 +1,14 @@
 import App from "./App";
-import Select from "../components/Select";
-import { mockPersonas } from "./mocks/data/personas.mocks";
 import { useDevToolsState, Switchboard } from "react-switchboard";
-import Field from "../components/Field";
 import { requestHandlers } from "./mocks/handlers/handlers";
-import useUserSync from "./useUserSync";
-import Button from "../components/Button";
-import { addTodo, deleteTodo } from "./apis/todo-apis";
-import Input from "../components/Input";
-import { useState } from "react";
 import ErrorFallback from "./ErrorFallback";
-
-type SimulateAction = "add" | "edit" | "delete";
+import { User } from "./User";
+import { SimulateTraffic } from "./SimulateTraffic";
 
 export default function AppWithDevTools() {
   // Storing only userId in devToolsState to keep localStorage and URL minimal.
   // Storing the entire persona would bloat localStorage and the URL.
   const [userId, setUserId] = useDevToolsState<number | "">("userId", "");
-  useUserSync(userId, setUserId);
-
-  const [simulateAction, setSimulateAction] = useState<SimulateAction>("add");
-  const [simulateAdd, setSimulateAdd] = useState("");
-  const [simulateDelete, setSimulateDelete] = useState("");
 
   return (
     <Switchboard
@@ -30,8 +17,7 @@ export default function AppWithDevTools() {
         requestHandlers,
         startOptions: {
           // Don't log mocked requests to the browser console.
-          quiet: true,
-
+          quiet: false,
           // Ignore unhandled requests. Uncomment below to throw errors for unhandled requests instead.
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
           // onUnhandledRequest: ({ method, url }) => {
@@ -47,80 +33,8 @@ export default function AppWithDevTools() {
       // Using a key to reinitialize the app when the userId changes.
       appSlot={<App key={userId} />}
     >
-      <Field>
-        <Select
-          width="full"
-          id="user"
-          label="Persona"
-          value={userId}
-          onChange={(e) => {
-            setUserId(e.target.value ? parseInt(e.target.value) : "");
-          }}
-        >
-          <option value="">Logged out</option>
-          {mockPersonas.map(({ id, description, response }) => (
-            <option value={id} key={id}>
-              {response.name} ({description.role}, {description.todos})
-            </option>
-          ))}
-        </Select>
-      </Field>
-
-      <details className="mt-4" open>
-        <summary className="font-bold">Simulate Traffic</summary>
-        <Field>
-          <Select
-            label="Action"
-            value={simulateAction}
-            onChange={(e) => {
-              setSimulateAction(e.target.value as SimulateAction);
-            }}
-          >
-            <option value="add">Add</option>
-            <option value="delete">Delete</option>
-            <option value="edit">Edit</option>
-          </Select>
-        </Field>
-
-        {simulateAction === "add" && (
-          <Field>
-            <Input
-              label="Todo"
-              value={simulateAdd}
-              id="simulate-add"
-              onChange={(e) => setSimulateAdd(e.target.value)}
-            />
-            <Button
-              onClick={() => {
-                addTodo(simulateAdd);
-              }}
-            >
-              Add
-            </Button>
-          </Field>
-        )}
-
-        {simulateAction === "delete" && (
-          <Field>
-            <Input
-              label="Todo Id"
-              value={simulateDelete}
-              type="number"
-              id="simulate-delete"
-              onChange={(e) => setSimulateDelete(e.target.value)}
-            />
-            <Button
-              onClick={() => {
-                if (!simulateDelete) return;
-                deleteTodo(parseInt(simulateDelete));
-                setSimulateDelete("");
-              }}
-            >
-              Delete
-            </Button>
-          </Field>
-        )}
-      </details>
+      <User userId={userId} setUserId={setUserId} />
+      <SimulateTraffic />
     </Switchboard>
   );
 }
