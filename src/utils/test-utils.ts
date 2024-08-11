9634eda67e8506af3104fef4321ee6d4c703ffcb
Cory House <housecor@gmail.com>
2024-08-11 08:05:07 -0500

    centralize test utils

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -12,6 +12,21 @@ const customRender = (ui: React.ReactElement, options = {}) =>
     ...options,
   });
 
+/** Build a URL that contains a querystring key/value pair for each
+ * populated property in the provided config. By convention, each property
+ * name is mapped to the querystring's key.
+ */
+export function buildUrl<TDevToolsConfig>(
+  baseUrl: string,
+  config: Partial<TDevToolsConfig>
+) {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(config)) {
+    params.append(key, JSON.stringify(value));
+  }
+  return baseUrl + "?" + params.toString();
+}
+
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
 // override render export
