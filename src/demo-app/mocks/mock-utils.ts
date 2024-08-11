752450830d7d11a491f37593b3e6db67f6edfcee
Cory House <housecor@gmail.com>
2024-08-11 07:51:09 -0500

    remove unused

--- src/demo-app/mocks/mock-utils.ts
+++ src/demo-app/mocks/mock-utils.ts
@@ -1,18 +1,7 @@
 import { delay } from "msw";
-import { CustomResponse } from "../../types/types";
-import { Handler, RequestHandlerConfig } from "../demo-app-types";
+import { Handler } from "../demo-app-types";
 import { mockPersonas } from "./data/personas.mocks";
-
-// Returns the endpoints delay if one is specified
-// Falls back to global delay if one is specified.
-// Returns 0 otherwise.
-export function getDelay(
-  configRef: React.MutableRefObject<RequestHandlerConfig>,
-  endpointDelay?: number
-) {
-  if (endpointDelay) return endpointDelay;
-  return configRef.current?.delay ?? 0;
-}
+import { userIdKey } from "../constants/localStorage.constants";
 
 export function getCustomResponseSettings(handler: Handler) {
   const customResponsesAsString = localStorage.getItem("customResponses");
