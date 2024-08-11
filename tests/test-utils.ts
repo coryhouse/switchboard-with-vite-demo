c2510eee76ed44fb2d819f84dbb39d9554b45db6
Cory House <housecor@gmail.com>
2024-08-11 12:33:03 -0500

    Streamline config

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,17 +1,3 @@
-import { cleanup, render } from "@testing-library/react";
-import { afterEach } from "vitest";
-
-afterEach(() => {
-  cleanup();
-});
-
-const customRender = (ui: React.ReactElement, options = {}) =>
-  render(ui, {
-    // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
-    ...options,
-  });
-
 /** Build a URL that contains a querystring key/value pair for each
  * populated property in the provided config. By convention, each property
  * name is mapped to the querystring's key.
@@ -26,8 +12,3 @@ export function buildUrl<TDevToolsConfig>(
   }
   return baseUrl + "?" + params.toString();
 }
-
-export * from "@testing-library/react";
-export { default as userEvent } from "@testing-library/user-event";
-// override render export
-export { customRender as render };
