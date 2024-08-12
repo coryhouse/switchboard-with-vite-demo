593c4be74de0a89ab27f0da057533e4fb7e03580
Cory House <housecor@gmail.com>
2024-08-11 22:17:37 -0600

    include delay

--- src/demo-app/mocks/mock-utils.ts
+++ src/demo-app/mocks/mock-utils.ts
@@ -4,24 +4,32 @@ import { mockPersonas } from "./data/personas.mocks";
 import { userIdKey } from "../constants/localStorage.constants";
 import { CustomResponse } from "react-switchboard";
 
-export function getCustomResponseSettings(handler: Handler) {
+// TODO: Move helper for all/some of this to Switchboard?
+export async function getCustomResponseSettings(handler: Handler) {
   const customResponsesAsString = localStorage.getItem("sb-customResponses");
   if (!customResponsesAsString) return null;
   // TODO: Use Zod to validate and type this data.
   const customResponses = JSON.parse(
     customResponsesAsString
   ) as CustomResponse[];
-  return customResponses.find((r) => r.handler === handler);
+
+  const customResponseSettings = customResponses.find(
+    (r) => r.handler === handler
+  );
+
+  const globalDelay = localStorage.getItem("sb-delay");
+
+  // If a delay is configured, apply it before returning settings so we automatically handle delay for all requests.
+  // Apply the custom response's delay, if any, the global delay, if any, or fallback to 0 if neither.
+  await delay(
+    customResponseSettings?.delay ?? (globalDelay ? parseInt(globalDelay) : 0)
+  );
+
+  return customResponseSettings;
 }
 
 export function getUser() {
   const userId = localStorage.getItem(userIdKey);
   const user = mockPersonas.find((u) => u.id === parseInt(userId!));
   return user;
-}
-
-// TODO: Move helper to Switchboard?
-export async function delayResponse() {
-  const waitFor = parseInt(localStorage.getItem("sb-delay") ?? "") || 0;
-  await delay(waitFor);
 }
